46abd14809c43161a47b6446933bcba833937bdf
Faisal Raza <faisalrazaqazi2000@gmail.com>
2024-09-26 12:17:51 +0500

    done

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ dotenv.config({
 });
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173' || "https://geminisphere.netlify.app",
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
