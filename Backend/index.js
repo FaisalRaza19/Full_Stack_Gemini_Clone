11fd9fff95823a524770a5ba662de362703c3f08
Faisal Raza <faisalrazaqazi2000@gmail.com>
2024-10-13 18:01:06 +0500

    done

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ dotenv.config({
 
 const corsOptions = {
     origin: (origin, callback) => {
-        const allowedOrigins = ['http://localhost:5173', 'https://geminisphere.netlify.app', 'https://full-stack-gemini-clone.vercel.app/'];
+        const allowedOrigins = ['http://localhost:5173'];
 
         if (allowedOrigins.includes(origin) || !origin) {
             callback(null, true);
